9b1460b8f9e49600f98115d0e028f3a1f76ed4fb
Christof Wittmann <c.wittmann@it-labs.de>
2019-12-08 16:12:12 +0100

    Alert with response.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function postData(input) {
 
 function callbackFunc(response) {
     // do something with the response
-    console.log(response);
+    alert(response);
 }
 
 // Take a picture when cameraTrigger is tapped
