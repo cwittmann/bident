d33a2e9a75785a81465775f4ebdeca2a4860b092
Christof Wittmann <c.wittmann@it-labs.de>
2020-01-02 22:28:21 +0100

    feat: removed camera output element

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
+    cameraTrigger = document.querySelector("#camera--trigger")
 
 function cameraStart() {
     if (window.stream) {
