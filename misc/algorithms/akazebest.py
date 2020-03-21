f2f27b117ec4a7a95806519483c4fc5fbdc2064c
Christof Wittmann <c.wittmann@it-labs.de>
2020-03-21 22:08:56 +0100

    BA

--- misc/algorithms/akazebest.py
+++ misc/algorithms/akazebest.py
@@ -31,19 +31,6 @@ for m,n in matches:
     if m.distance < 0.7*n.distance:
         good.append(m)
 
-if len(good)>MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
-    h,w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv.perspectiveTransform(pts,M)
-    img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
-else:
-    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
-    matchesMask = None
-
 img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None, flags = 2)
 
 endTime = time.time()
